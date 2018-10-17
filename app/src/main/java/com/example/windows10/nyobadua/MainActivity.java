783f180b7f38b2dfc9ea93e107530189b5567d3f
Kafiyatul Fithri <fiyakafiya@gmail.com>
2018-10-17 14:08:38 +0700

    Login regis

--- app/src/main/java/com/example/windows10/nyobadua/MainActivity.java
+++ app/src/main/java/com/example/windows10/nyobadua/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.windows10.nyobadua;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+    }
+
+    public void login(View v) {
+        Intent intent = new Intent(MainActivity.this,home.class);
+        MainActivity.this.startActivity(intent);
+    }
+
+    public void signup(View v) {
+        Intent intent = new Intent(MainActivity.this,regis.class);
+        MainActivity.this.startActivity(intent);
     }
 }
