0a316b979e0542fa44d2576ff0020dc54b4560de
Unknown <naluthfi@gmail.com>
2018-10-18 05:41:51 +0700

    tes

--- app/src/main/java/com/example/windows10/nyobadua/home.java
+++ app/src/main/java/com/example/windows10/nyobadua/home.java
@@ -1,7 +1,7 @@
 package com.example.windows10.nyobadua;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class home extends AppCompatActivity {
 
--- app/src/main/java/com/example/windows10/nyobadua/regis.java
+++ app/src/main/java/com/example/windows10/nyobadua/regis.java
@@ -2,12 +2,25 @@ package com.example.windows10.nyobadua;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class regis extends AppCompatActivity {
-
+    private EditText regUsername;
+    private EditText regPasw;
+    private EditText regMhs1;
+    private EditText regMhs2;
+    private EditText regMhs3;
+    private EditText company;
+    private EditText dosen;
+    private Button btnsignup;
+    private EditText textView3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regis);
+        regUsername = (EditText) findViewById(R.id.regUsername);
+
+
     }
 }
